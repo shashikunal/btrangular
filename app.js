602a62de139c6f9597ff42edb969c057f0804a91
shashi <shashi@gmail.com>
2018-01-18 19:31:59 +0530

    validation

--- app.js
+++ app.js
@@ -1,56 +1,10 @@
 var app = angular.module("myApp" , ["ngRoute"]);
-app.controller("shashiCtr" ,["$scope" ,function($scope){
-    $scope.users = [
-        {
-            name:"shashi",
-            age:27,
-            doj:new Date("01/31/1988"),
-            salary:50000,
-            designation:"nodejs developer"
 
-         },
-         {
-            name:"raghu",
-            age:21,
-            doj:new Date("12/31/1990"),
-            salary:10000,
-            designation:"angularjs developer"
-
-         },
-         {
-            name:"vinay",
-            age:29,
-            doj:new Date("06/14/1985"),
-            salary:20000,
-            designation:"java developer"
-
-         }
-         
-]
-}]);
-
-
-app.filter("jspiderFilter" , function(){
-    return function(input , option){
-        if(isNaN(option) || option == ""){
-           return input;
-        }else {
-        return input.slice(0 , option).toUpperCase();
-        }
-    }
-
+app.service("random" , function(){
+    var  number = Math
 });
 
 
-app.config(["$routeProvider" , "$locationProvider" , function($routeProvider , $locationProvider){
-    $routeProvider.when("/login" , {
-        templateUrl : "views/login.html"
-    }).when("/register" , {
-        templateUrl : "views/register.html"
-    }).otherwise({
-        redirectTo:"/"
-    });
-}])
 
 
 
--- route/app.js
+++ route/app.js
@@ -8,6 +8,9 @@ myApp.config(["$routeProvider" ,function($routeProvider){
         templateUrl:"/views/success.html"
     }).when("/home" , {
         templateUrl:"/views/home.html"
+    })
+    .when("/contact" , {
+        templateUrl:"/views/contact.html"
     }).otherwise({
         redirectTo:"/login"
     });
@@ -18,3 +21,44 @@ myApp.run(function($rootScope){
 })
 
 
+myApp.controller("serviceCtr" , function($scope , $timeout , $interval){
+
+$scope.sometext = "we are learning angularjs";
+$timeout(function(){
+$scope.sometext = "we are learning Nodejs";
+
+},5000);
+
+
+$scope.time = new Date().toLocaleTimeString();
+$interval(function(){
+    $scope.time = new Date().toLocaleTimeString();
+},1000)
+});
+
+
+myApp.directive("jspiders" , function(){
+    return {
+        restrict:"A",
+        templateUrl:"/views/register.html",
+        link:function(scope , element){
+            element.bind("mouseenter" , function(){
+                console.log("touched");
+            })
+        }
+
+    }
+});
+
+myApp.directive("qspiders" , function(){
+    return {
+        restrict:"A",
+        templateUrl:"/views/register.html",
+        link:function(scope , element){
+            element.bind("mouseleave" , function(){
+                console.log("leave");
+            });
+        }
+
+    }
+})
